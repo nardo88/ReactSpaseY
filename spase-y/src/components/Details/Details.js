b356e7ffaa093934ba54e235820b0fe3713d9a75
nardo88 <nardo1988@mail.ru>
2021-02-07 18:01:25 +0300

    finish

--- spase-y/src/components/Details/Details.js
+++ spase-y/src/components/Details/Details.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import Main from '../Main/Main';
 import './details.css';
 import useLaunches from '../useLaunches/useLaunches';
+import Youtube from 'react-youtube';
 
 const Details = (props) => {
     const [launch, setLaunch] = useState(null);
@@ -14,25 +15,26 @@ const Details = (props) => {
     const history = useHistory();
 
     console.log(launch);
+
+    if(!launch) return null;
+
     return (
         <>
-        <Main />
-        <main className="details">
-		<div className="container">
-                <div className="details-row">
-                    <div className="details-image">
-                        <img src="https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png" alt="" />
-                    </div>
-                    <div className="details-content">
-                        <p className="details-description">Engine failure at 33 seconds and loss of vehicle</p>
-                    </div>
+            <Main rocket={launch.name} />
+            <main className="details">
+                <div className="container">
+                        <div className="details-row">
+                            <div className="details-image">
+                                <img src={launch.links.patch.small} alt={launch.name} />
+                            </div>
+                            <div className="details-content">
+                                <p className="details-description">{launch.details}</p>
+                            </div>
+                            <Youtube className="details-youtube" videoId={launch.links.youtube_id} />
+                        </div>
                 </div>
-                <div>
-                    <iframe className="details-youtube" width="560" height="315" src="https://www.youtube.com/embed/dLQ2tZEH6G0" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-                </div>
-            </div>
-            <a onClick={history.goBack} className="button button-back">go back</a>
-        </main>
+                <a onClick={history.goBack} className="button button-back">go back</a>
+            </main>
         </>
     )
 }
